42a9ad3f1e29ad7478af355cab0d1dd19edc7d17
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2020-12-11 18:38:46 +0000

    VS Project v0.3

---
+++ VS Project/SafetyTourism/SafetyTourism/Data/SafetyContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SafetyTourism.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SafetyTourism.Data
+{
+    public class SafetyContext : DbContext
+    {
+        public SafetyContext(DbContextOptions<SafetyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Utilizador> Utilizadores { get; set; }
+        public DbSet<Destino> Destinos { get; set; }
+        public DbSet<Relatorio> Relatorios { get; set; }
+    }
+}
