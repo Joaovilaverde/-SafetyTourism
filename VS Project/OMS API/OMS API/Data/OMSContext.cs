4adc55af57c0610cd01f56bb5af9360503f9be65
Raphael Silva <raphael.silva.rdcs@upskill.pt>
2020-12-21 10:04:15 +0000

    Surto

---
+++ VS Project/OMS API/OMS API/Data/OMSContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OMS_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace OMS_API.Data
+{
+    public class OMSContext :DbContext
+    {
+        public OMSContext(DbContextOptions<OMSContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ VS Project/OMS API/OMS API/Models/Surto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OMS_API.Models
+{
+    public class Surto
+    {
+        public long SurtoID { get; set; }
+        public long VirusID { get; set; }
+        [ForeignKey ("VirusID")]
+        public Virus Virus { get; set; }
+        public long ZonaID { get; set; }
+        [ForeignKey("ZonaID")]
+        public Zona Zona { get; set; }
+        public DateTime DataDetecao { get; set; }
+        public DateTime DataFim { get; set; }
+    }
+}
