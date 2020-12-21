de4bb5b0cf5d4494c8b516f845c1f3fcb31aa2e8
jose-pedro-franco <jpmfranco@upskill.pt>
2020-12-21 10:02:35 +0000

    Classe Pais

---
+++ VS Project/OMS API/OMS API/Models/Pais.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OMS_API.Models
+{
+    public class Pais
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public long ZonaId { get; set; }
+        [ForeignKey("ZonaId")]
+        public Zona Zona { get; set; }
+
+    }
+}
