061e38c691e934a545d01340ee77a82c7d468a3e
Daniel Macedo <dcmacedo02@upskill.pt>
2020-12-21 10:05:25 +0000

    Recomendação

---
+++ VS Project/OMS API/OMS API/Models/Recomendacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OMS_API.Models {
+    public class Recomendacao {
+        public long Id { get; set; }
+        public long? ZonaId { get; set; }
+
+        [ForeignKey("ZonaId")]
+        public Zona Zona { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public long Validade { get; set; }
+    }
+}
