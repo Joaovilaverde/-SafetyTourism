b1263ddfcbd699f8dd37e8cbf01e54143dc7e1f0
Ines Oliveira <ines.machado.oliveira@upskill.pt>
2020-12-27 16:24:55 +0000

    Virus Controller Details + Create

--- VS Project/SafetyTourism v2/SafetyTourism/Controllers/VirusController.cs
+++ VS Project/SafetyTourism v2/SafetyTourism/Controllers/VirusController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using SafetyTourism.Models;
 using Microsoft.Extensions.Configuration;
 using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace SafetyTourism.Controllers
 {
@@ -60,5 +63,64 @@ namespace SafetyTourism.Controllers
                 int pageSize = 10;
                 return View(await PaginatedList<Virus>.CreateAsync(virus, pageNumber ?? 1, pageSize));
             }
+
+            //DETAILS
+            public async Task<IActionResult> Details(long? id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+                Virus virus;
+                using (HttpClient client = new HttpClient())
+                {
+                    string endpoint = apiBaseUrl + "/virus/" + id;
+                    var response = await client.GetAsync(endpoint);
+                    response.EnsureSuccessStatusCode();
+                    virus = await response.Content.ReadAsAsync<Virus>();
+                }
+                if (virus == null)
+                {
+                    return NotFound();
+                }
+                return View(virus);
+            }
+
+        // CREATE GET
+
+        [Authorize(Roles = "Funcionario,Administrador")]
+
+        public async Task<IActionResult> Create()
+        {
+            var listaVirus = new List<Virus>();
+            using (HttpClient client = new HttpClient())
+            {
+                string endpoint = apiBaseUrl + "/virus";
+                var response = await client.GetAsync(endpoint);
+                response.EnsureSuccessStatusCode();
+                listaVirus = await response.Content.ReadAsAsync<List<Virus>>();
+            }
+            return View();
         }
+
+        //CREATE POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Funcionario,Administrador")]
+        
+        public async Task<IActionResult> Create([Bind("Id,Nome")] Virus virus)
+        { 
+            if (ModelState.IsValid)
+            { 
+                using (HttpClient client = new HttpClient())
+                {
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(virus), Encoding.UTF8, "application/json");
+                    string endpoint = apiBaseUrl + "/virus";
+                    var response = await client.PostAsync(endpoint, content);
+                }
+            return RedirectToAction(nameof(Index));
+            }
+            return View(virus);
+        }       
+    }
 }
